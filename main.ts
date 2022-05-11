74e95d4958a021d6d4d51417b2a9748d40c488e7
Brandon <Brandonsexton14@gmail.com>
2022-05-12 00:31:48 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,8 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 100)
-        basic.showLeds(`
-            # # # # #
-            # . # . #
-            # . # . #
-            # . . # #
-            # # # # #
-            `)
     } else {
-        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 0)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 100)
     }
 })
 input.onButtonPressed(Button.A, function () {

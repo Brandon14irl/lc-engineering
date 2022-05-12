07bb4cb8e364b631496f622c19a334b1337e433f
Brandon <Brandonsexton14@gmail.com>
2022-05-12 01:10:08 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 100)
     } else {
-        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 100)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 0)
     }
 })
 input.onButtonPressed(Button.A, function () {

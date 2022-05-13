5a2d6aa1cfbd3d9c0971dc346645fd0a878871e1
Brandon <Brandonsexton14@gmail.com>
2022-05-13 20:51:37 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,23 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 100)
+    } else {
+        if (receivedNumber == 2) {
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, 100)
+        } else {
+            if (receivedNumber == 3) {
+                Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, 0)
+            }
+        }
     }
-})
-input.onButtonPressed(Button.A, function () {
-    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 100)
 })
 basic.forever(function () {
     radio.setGroup(1)
-    radio.setTransmitPower(6)
-    radio.setFrequencyBand(80)
+    basic.showLeds(`
+        . . # # .
+        . # . . .
+        # . . . .
+        # . . . .
+        . # # . .
+        `)
 })

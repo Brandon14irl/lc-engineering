0cbf9f04abe65a157a17d5e3ba968e32ff2729a6
Brandon <Brandonsexton14@gmail.com>
2022-05-15 22:06:59 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,29 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, 8)
-    } else if (receivedNumber == 2) {
-        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 79)
-    } else {
-        if (receivedNumber == 3) {
-            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, 0)
-        }
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, speed)
+    }
+    if (receivedNumber == 2) {
+        servos.P0.setAngle(90)
+    }
+    if (receivedNumber == 3) {
+        servos.P0.setAngle(90)
+    }
+    if (receivedNumber == 4) {
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, speed)
+    }
+    // DRS - if button is turned on motor 2 will turn for a second turned off it will go back to its original position
+    if (receivedNumber == 5) {
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor4, Kitronik_Robotics_Board.MotorDirection.Forward, 0)
+    }
+    if (receivedNumber == 6) {
+        speed += -10
+    }
+    if (receivedNumber == 7) {
+        speed += 10
     }
 })
+let speed = 0
+speed = 0
 music.playTone(175, music.beat(BeatFraction.Quarter))
 basic.pause(5)
 music.playTone(294, music.beat(BeatFraction.Quarter))
@@ -17,13 +32,7 @@ music.playTone(523, music.beat(BeatFraction.Quarter))
 basic.pause(5)
 music.playTone(988, music.beat(BeatFraction.Half))
 basic.pause(10)
+pins.digitalWritePin(DigitalPin.P14, 1)
 basic.forever(function () {
-    radio.setGroup(1)
-    basic.showLeds(`
-        . # # # .
-        # . . . .
-        # . . . .
-        # . . . .
-        . # # # .
-        `)
+    radio.setGroup(80)
 })

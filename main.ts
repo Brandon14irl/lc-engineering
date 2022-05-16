898811d29b2c29d68402c0ff04c8309022f4d39b
Brandon <Brandonsexton14@gmail.com>
2022-05-16 22:08:38 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,35 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, speed)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 40)
     }
     // steering
     if (receivedNumber == 2) {
-        servos.P0.setAngle(90)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, 40)
     }
     // steering
     if (receivedNumber == 3) {
-        servos.P0.setAngle(90)
+    	
     }
     if (receivedNumber == 4) {
-        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, speed)
+    	
     }
     // DRS - if button is turned on motor 4 will turn for a second, when turned off it will go back to its original position
     if (receivedNumber == 5) {
-        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor4, Kitronik_Robotics_Board.MotorDirection.Forward, 0)
+    	
     }
     if (receivedNumber == 6) {
-        speed += -10
+    	
     }
     if (receivedNumber == 7) {
-        speed += 10
+    	
     }
+    // steering
+    if (receivedNumber == 8) {
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 0)
+    }
+    basic.showNumber(receivedNumber)
 })
-let speed = 0
-speed = 0
+let speed = 1
 music.playTone(175, music.beat(BeatFraction.Quarter))
 basic.pause(5)
 music.playTone(294, music.beat(BeatFraction.Quarter))
